40b14cdcbe2349a681b7ce0dc4379b6f6a692545
ndthinh02 <thinhjerry1410@gmail.com>
2022-10-21 14:04:36 +0700

    add update

--- routes/index.js
+++ routes/index.js
@@ -7,95 +7,138 @@ var mongo = require('mongoose')
 const {json} = require("express");
 
 mongo.connect(db).catch(err => {
-  console.log("Da xay ra loi" + err)
+    console.log("Da xay ra loi" + err)
 })
 var foodSchema = new mongo.Schema({
-  id: 'String',
-  name: 'String',
-  urlImage: 'String',
-  type: 'String',
-  total: 'String',
-  price: 'String'
+    id: 'String',
+    name: 'String',
+    urlImage: 'String',
+    type: 'String',
+    total: 'String',
+    price: 'String'
 })
 
 var Food = mongo.model('food', foodSchema);
 var Cake = mongo.model('cake', foodSchema);
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 router.get('/getAll', function (req, res) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET');
-  Food.find({}, (err, data) => {
-    res.send((data))
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    Food.find({}, (err, data) => {
+        res.send((data))
 
-  })
+    })
 })
 router.get('/getCake', function (req, res) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET');
-  Food.find({
-    type: "1"
-  }, (err, data) => {
-    res.send((data))
-  })
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    Food.find({
+        type: "1"
+    }, (err, data) => {
+        res.send((data))
+    })
 })
 router.get('/getDrink', function (req, res) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET');
-  Food.find({
-    type: "2"
-  }, (err, data) => {
-    res.send((data))
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    Food.find({
+        type: "2"
+    }, (err, data) => {
+        res.send((data))
 
-  })
+    })
 })
 router.get('/getFruit', function (req, res) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET');
-  Food.find({
-    type: "3"
-  }, (err, data) => {
-    res.send((data))
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    Food.find({
+        type: "3"
+    }, (err, data) => {
+        res.send((data))
 
-  })
+    })
 })
 router.post("/addFood", (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    var id = req.body.id;
+    var name = req.body.name;
+    var total = req.body.total;
+    var type = req.body.type;
+    var price = req.body.price;
+    var url = req.body.urlImage;
 
-  var id = req.body.id;
-  var name = req.body.name;
-  var total = req.body.total;
-  var type = req.body.type;
-  var price = req.body.price;
-  var url = req.body.urlImage;
-
-  const food = new Food({
-    id: id,
-    urlImage: url,
-    total: total,
-    name:name,
-    price:price,
-    type:type
-  })
-  food.save(function (err) {
-    res.redirect('/getAll')
-  })
+    const food = new Food({
+        id: id,
+        urlImage: url,
+        total: total,
+        name: name,
+        price: price,
+        type: type
+    })
+    food.save(function (err) {
+        res.redirect('/getAll')
+    })
 })
 router.get('/addFood', (req, res) => {
-
-  res.render('addPost')
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.render('addPost')
 
 })
 router.get('/delete', function (req, res, next) {
-
-  Food.find({}, (err, data) => {
-    res.render('delete', {data: data});
-  })
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    Food.find({}, (err, data) => {
+        res.render('delete', {data: data});
+    })
 });
+router.post("/updateFood", (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    var id = req.body.id;
+    var name = req.body.name;
+    var total = req.body.total;
+    var type = req.body.type;
+    var price = req.body.price;
+    var url = req.body.urlImage;
+    console.log(id)
+    Food.findOneAndUpdate({_id:id}, {urlImage: url, total: total, name: name, price: price, type: type}, (err, data) => {
+        res.redirect('/delete')
+        console.log(data)
+    })
+
+
+})
+
+router.post('/update', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    var idUpdate = req.body.idUpdate;
+    Food.findById(idUpdate, (err, data) => {
+        res.render('update', {data: data})
+        // console.log(data)
+    })
+})
+router.get('/update', function (req, res, next) {
+    var idUpdate = req.body.idUpdate;
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    Food.find({id: idUpdate}, (err, data) => {
+        res.render('update', {data: data});
+    })
+
+});
+
 router.post("/delete", (req, res) => {
-  var id = req.body.idDelete;
-  Food.findByIdAndRemove(id, (err, data) => {
-    res.redirect('/delete')
-  })
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    var id = req.body.idDelete;
+
+    Food.findByIdAndRemove(id, (err, data) => {
+        res.redirect('/delete')
+    })
 })
 module.exports = router;
